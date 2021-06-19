353944adb862335a4773315d3d9171b4eb8e00b9
Shateizin <86153752+Shateizin@users.noreply.github.com>
2021-06-19 20:53:06 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from flask import Flask
+from bs4 import BeautifulSoup as bs
+
+app = Flask(__name__)
+
+
+@app.route('/cp/<string:cpf>/')
+def consul_cpf(cpf):
+	url = requests.get(f"http://3.223.192.184/CPF/api.php?lista={cpf}").text
+	soup = bs(url, 'html.parser')
+	if url == "":
+		return "CPF: NÃO VALIDO"
+	else:
+		return 'CPF: EXISTENTE'
+
+def main():
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
+	
+	
+if __name__ == "__main__":
+	main()
