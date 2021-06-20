fb21194f4ab027a535c5e02f949a8dc4827075d9
Shateizin <86153752+Shateizin@users.noreply.github.com>
2021-06-20 17:10:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import os
 import requests
 from flask import Flask
 from bs4 import BeautifulSoup as bs
-
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/cp/<string:cpf>/')
 def consul_cpf(cpf):
