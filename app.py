9d80e2c96bb97b914b680da9304d5f66a6eb672c
Shateizin <86153752+Shateizin@users.noreply.github.com>
2021-06-22 08:32:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Flask
+from flask import Flask, jsonify
 from bs4 import BeautifulSoup as bs
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
@@ -15,7 +15,7 @@ def consul_cpf(cpf):
 	else:
 		resul = soup.find("b").text
 		resul = resul.replace("NOME", "").replace("CONSULTADO COM SUCESSO", "").replace("➜", "").replace("|", "").replace("SEXO", "").replace("NASCIMENTO", "").replace(":", "").replace("IDADE", "").replace("CPF", "").replace("DATA DA CONSULTA", "").replace("Seg", "").replace("MASCULINO", "").replace("3", "").replace("2", "").replace("/", "").replace("1", "").replace("4", "").replace("5", "").replace("6", "").replace("7", "").replace("8", "").replace("9", "").replace("FEMININO", "").replace("0", "")
-		return "{'status': 'EXISTENTE', 'nome': '" + resul + "'}"
+		return jsonify({"status": "EXISTENTE", "nome": resul})
 
 def main():
 	port = int(os.environ.get("PORT", 5000))
