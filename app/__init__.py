32173babae6280cd4b285c6a71f774c57dac853d
Shateizin <86153752+Shateizin@users.noreply.github.com>
2022-04-06 08:20:00 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask
-
+import os
+import requests
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup as bs
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/solana/<string:address>/')
 def consul_nft(address):
